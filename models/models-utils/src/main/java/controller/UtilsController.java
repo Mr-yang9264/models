a4e0d9f0d83e35a9a01372e343f337ef96fd41fd
yang <yang_fei9264@163.com>
2021-12-09 14:57:18 +0800

    other 分支提交1

--- models/models-utils/src/main/java/controller/UtilsController.java
+++ models/models-utils/src/main/java/controller/UtilsController.java
@@ -22,5 +22,6 @@ public class UtilsController {
     @GetMapping("/other")
     public void addFunction(){
         System.out.println("嘻嘻嘻嘻嘻嘻嘻嘻嘻嘻嘻嘻嘻嘻嘻");
+        System.out.println("哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈");
     }
 }
